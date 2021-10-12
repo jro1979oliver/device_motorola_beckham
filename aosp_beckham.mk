78d2c94e2f32bae48250b7d35f5f1e63522c2fcc
Jorge Lucas <jorgelucas.sd@gmail.com>
2021-10-12 12:54:26 -0300

    beckham: Disable some google apps to save space
    Change-Id: Ia3253998ebf595609f4d332b2fad4128d4b10d1d

--- aosp_beckham.mk
+++ aosp_beckham.mk
@@ -28,8 +28,8 @@ $(call inherit-product, device/motorola/beckham/device.mk)
 # Inherit some common Pixel Experience stuff.
 TARGET_GAPPS_ARCH := arm64
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_INCLUDE_STOCK_ARCORE := true
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_SUPPORTS_GOOGLE_RECORDER := false
 
 # A/B updater
 AB_OTA_POSTINSTALL_CONFIG += \
